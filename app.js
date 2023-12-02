169a8c249571e1ef09ec99f78e0ddb4aafa5be17
marlissonsilva <marlyssonsilva005@gmail.com>
2023-12-02 01:32:48 -0300

    added cors

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import connectDatabase from "./src/database/db.js";
 import router from "./src/routes/index.js";
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors())
+
 app.use(bodyParser.json());
+
 connectDatabase();
+
 app.use(router);
+
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}/doc`);
 });
